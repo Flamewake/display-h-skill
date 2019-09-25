52ca4c51e031d2313208e9ac8a94dabe5947308c
Andrew G <55808510+Flamewake@users.noreply.github.com>
2019-09-25 15:31:12 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,16 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class DisplayH(MycroftSkill):
+    SEC_PER_LETTER = 0.9         # based on the Mark 1 scrolling speed
+    LETTERS_PER_SCREEN = 7.0     # based on the Mark 1 screen size
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('h.display.intent')
     def handle_h_display(self, message):
         self.speak_dialog('h.display')
+                self.enclosure.mouth_text('The meaning of life, the universe and everything is 42')
 
 
 def create_skill():
