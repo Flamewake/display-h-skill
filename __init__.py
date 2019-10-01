8fce95fd846c4f24294da187293e4ba92265592f
Andrew G <55808510+Flamewake@users.noreply.github.com>
2019-10-01 14:57:10 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
-from mycroft import MycroftSkill, intent_handler, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
+from mycroft.util.log import LOG
 
 
 class DisplayH(MycroftSkill):
     def __init__(self):
         super(DisplayH, self).__init__(name="DisplayH")
 
-    @intent_handler(IntentBuilder("HIntent").require("HKeyword.voc"))
-    def handle_h_display(self, message):
+    @intent_handler(IntentBuilder("HIntent").require("HKeyword"))
+    def handle_h_intent(self, message):
         self.speak_dialog('h.display')
         self.enclosure.mouth_text('H')
 
